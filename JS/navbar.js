0a75350c4b03f98fb6b5b01def67b7c4f424b70d
Ollie Jones <oathompsonjones@gmail.com>
2020-04-16 21:01:32 +0100

    stuff

--- JS/navbar.js
+++ JS/navbar.js
@@ -1,3 +1,4 @@
+document.write(/*html*/`
 <link rel="stylesheet" type="text/css" href="CSS/header.css">
 <div class="banner"></div>
 <ul>
@@ -6,30 +7,30 @@
     </li>
 
     <li>
-        <a href="/?page=news"><h2>News</h2></a>
+        <a href="/news"><h2>News</h2></a>
     </li>
 
     <li class="sub">
-        <a href="/?page=events"><h2>Events and Campaigns</h2></a>
+        <a href="/events"><h2>Events and Campaigns</h2></a>
         <div class="dropdown">
             <ul>
                 <li>
-                    <a href="/?page=events"><h3>Events</h3></a>
+                    <a href="/events"><h3>Events</h3></a>
                 </li>
 
                 <li>
-                    <a href="/?page=environment"><h3>Environment</h3></a>
+                    <a href="/environment"><h3>Environment</h3></a>
                 </li>
             </ul>
         </div>
     </li>
 
     <li class="sub" style="float: right;">
-        <a href="/?page=contact"><h2>Contact Us</h2></a>
+        <a href="/contact"><h2>Contact Us</h2></a>
         <div class="dropdown">
             <ul>
                 <li>
-                    <a href="/?page=contact"><h3>Contact Information</h3></a>
+                    <a href="/contact"><h3>Contact Information</h3></a>
                 </li>
 
                 <li>
@@ -42,4 +43,4 @@
             </ul>
         </div>
     </li>
-</ul>
+</ul>`);
