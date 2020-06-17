233d82abbe5cb86316756dca59dcf16ced2ddf11
Ollie Jones <oathompsonjones@gmail.com>
2020-06-17 16:18:04 +0100

    Update news.js

--- JS/news.js
+++ JS/news.js
@@ -1,13 +1,16 @@
 const news = [
     {
+        date: "Wednesday, 17 June 2020",
+        desc: "Our new YouTube channel launched today <a href=\"https://www.youtube.com/channel/UCSaaFS8LzdlEt4mFirY4-qg/featured\">@TBSHSInteract</a>.<br>We will be streaming quizzes weekly, open to everyone."
+    }, {
         date: "Thursday, 13 May 2020",
         desc: "This website launched today as a part of Interact's new and improved online image."
     }, {
         date: "Tuesday, 5 November 2019",
-        desc: "Interact's new Facebook page launched today <a href=\"https://fb.me/TBSHSInteract\">@TBSHSInteract</a>."
+        desc: "Our new Facebook page launched today <a href=\"https://fb.me/TBSHSInteract\">@TBSHSInteract</a>."
     }, {
         date: "Monday, 4 November 2019",
-        desc: "Interact's new Twitter feed launched today <a href=\"https://twitter.com/TBSHSInteract\">@TBSHSInteract</a>."
+        desc: "Our new Twitter feed launched today <a href=\"https://twitter.com/TBSHSInteract\">@TBSHSInteract</a>."
     }
 ];
 
