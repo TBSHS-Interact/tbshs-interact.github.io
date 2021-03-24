05cba8109c43b90f844874c71548d1292521058c
Ollie Jones <oathompsonjones@gmail.com>
2021-03-24 17:32:57 +0000

    Update contact.js

--- JS/contact.js
+++ JS/contact.js
@@ -1,20 +1,28 @@
-const personel = [
+const personnel = [
     {
         role: "Coordinator (Staff)",
-        names: [ "Simon Etheridge" ]
-    }, {
+        names: ["Simon Etheridge"],
+    },
+    {
         role: "President",
-        names: [ "Ollie Jones", "Dan Palmer" ]
-    }, {
-        role: "Events Manager",
-        names: [ "Archie Summerfield" ]
-    }, {
+        names: ["Ollie Jones", "Dan Palmer"],
+    },
+    {
+        role: "Vice President",
+        names: ["Sam Williams", "Kieran Carter"],
+    },
+    {
         role: "Secretary",
-        names: [ "Sam Williams" ]
-    }, {
+        names: ["Sam Williams"],
+    },
+    {
         role: "Treasurer",
-        names: [ "Kieran Carter" ]
-    }
+        names: ["Kieran Carter"],
+    },
+    {
+        role: "Events Manager",
+        names: ["Archie Summerfield"],
+    },
 ];
 
-document.write(personel.map((person) => `${person.names.length > 1 ? `${person.role}s` : person.role} - ${person.names.map((name) => `<a>${name}</a>`).join(" and ")}`).join("<br>"));
+document.write(personnel.map((person) => `${person.names.length > 1 ? `${person.role}s` : person.role} - ${person.names.map((name) => `<a>${name}</a>`).join(" and ")}`).join("<br>"));
