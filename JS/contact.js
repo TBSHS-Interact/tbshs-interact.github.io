ec16746475231a31cabca7cc4b54dbfc264c135a
Ollie Jones <oathompsonjones@gmail.com>
2021-03-24 17:34:38 +0000

    Update contact.js

--- JS/contact.js
+++ JS/contact.js
@@ -1,28 +1,32 @@
 const personnel = [
     {
         role: "Coordinator (Staff)",
-        names: ["Simon Etheridge"],
+        names: ["Simon Etheridge"]
     },
     {
         role: "President",
-        names: ["Ollie Jones", "Dan Palmer"],
+        names: ["Ollie Jones", "Dan Palmer"]
     },
     {
         role: "Vice President",
-        names: ["Sam Williams", "Kieran Carter"],
+        names: ["Sam Williams", "Kieran Carter"]
     },
     {
         role: "Secretary",
-        names: ["Sam Williams"],
+        names: ["Sam Williams"]
     },
     {
         role: "Treasurer",
-        names: ["Kieran Carter"],
+        names: ["Kieran Carter"]
+    },
+    {
+        role: "Public Relations",
+        names: ["Ollie Jones"]
     },
     {
         role: "Events Manager",
-        names: ["Archie Summerfield"],
-    },
+        names: ["Archie Summerfield"]
+    }
 ];
 
 document.write(personnel.map((person) => `${person.names.length > 1 ? `${person.role}s` : person.role} - ${person.names.map((name) => `<a>${name}</a>`).join(" and ")}`).join("<br>"));
